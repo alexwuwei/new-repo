2e802abd49207298365386dc43c5072ba3dfdb82
preetsohal <preetsohal@live.com>
2016-01-07 14:09:52 -0800

    updating data

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var user = prompt('what is your name?');
 alert('Hi there,'+user+', Let\s play some puzzles.');
 console.log('the user\'s name is' +user);
 
-
 var answer2 =prompt('Would you like to play a game? Yes or No').toUpperCase();
 if (answer2==='YES'|| answer2==='Y'){
     alert('Ok, let\'s continue');
@@ -12,17 +11,17 @@ if (answer2==='YES'|| answer2==='Y'){
     alert('Sorry, you must type Yes or NO')
 }
 
-
 var correct = false;
 while (correct===false){
   var answer2 = prompt (user + ', Can you guess my Age?');
-  console.log('The user answers question 1:'+ answer1);
-if (answer2 <=29) {
-      alert('Too low.');
- } else if (answer1 >=31) {
-  alert('too high');
-} else {
-  alert('That\'s correct');
-  correct = true;
-}
+  console.log('The user answers question 1:'+ answer2);
+    if (answer2 <=29) {
+          alert('Too low.');
+
+     } else if (answer2 >=31) {
+      alert('too high');
+    } else {
+      alert('That\'s correct');
+      correct = false;
+    }
 }
