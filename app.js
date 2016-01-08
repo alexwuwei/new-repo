2b19cde2c71a4e8e6f8fe916599870d5ef5c4f7d
Hugo A Guerra <guerrahugoa@gmail.com>
2016-01-08 13:43:29 -0800

    Created single function for handling all questions. Also created needed array for this purpose

--- app.js
+++ app.js
@@ -11,61 +11,70 @@ if (answer2==='YES'|| answer2==='Y'){
     alert('Sorry, you must type Yes or NO')
 }
 var res1 = document.getElementById('resultOne');
-
-function firstQuestion() {
-  var correct = false;
-  while (correct===false){
-    var answer2 = prompt (user + ', Can you guess my Age?');
-    console.log('The user answers question 1:'+ answer2);
-      if (answer2 <=29) {
-            res1.textContent = 'Too low.';
-
-       } else if (answer2 >=31) {
-        res1.textContent = 'too high';
-      } else {
-        res1.textContent = 'That\'s correct';
-        correct = true;
-      }
-  }
-}
-
-firstQuestion()
-
 var res2 = document.getElementById('resultTwo');
-
-function secondQuestion() {
-  var ans2 = prompt('Do I have a cat?');
-  if (ans2.toUpperCase() === 'Y' || ans2.toUpperCase() === 'YES') {
-    res2.textContent = 'No, I do not have a cat.';
-  } else {
-    res2.textContent = 'You are right.'
-  }
-}
-
-secondQuestion()
-
 var res3 = document.getElementById('resultThree');
-
-function thirdQuestion() {
-  var ans3 = prompt('Do I live in Kent?');
-  if (ans3.toUpperCase() === 'Y' || ans3.toUpperCase() === 'YES') {
-    res3.textContent = 'You are right.';
-  } else {
-    res3.textContent = 'You are wrong.';
-  }
-}
-
-thirdQuestion()
-
 var res4 = document.getElementById('resultFour');
 
-function fourthQuestion() {
-  var ans4 = prompt('Was I born in Seattle?')
-  if (ans4.toUpperCase() === 'Y' || ans4.toUpperCase() === 'YES') {
-    res4.textContent = 'No, I was born in India';
-  } else {
-    res4.textContent = 'You are right.';
-  }
+
+var quesData = [ ['Am I 30 years old?', 'Y', 'YES', 'That is correct', 'You are so wrong oh my god', res1], ['Do I have a cat?', 'N', 'NO', 'You are right', 'You are wrong', res2], ['Do I live in Kent?', 'Y', 'YES', 'You are right', 'You are wrong', res3] ];
+
+
+
+for (var i = 0; i < quesData.length; i++) {
+  // questions();
+  questions()
 }
 
-fourthQuestion()
+function questions () {
+
+  var answer = prompt(quesData[i] [0]);
+  if (answer.toUpperCase() === quesData[i] [1] || answer.toUpperCase() === quesData[i] [2]) {
+    //correct
+    console.log("answer was right");
+    quesData[i] [5].textContent = quesData[i] [3];
+  } else {
+    //wrong answer
+    console.log("answer was wrong");
+    quesData[i] [5].textContent = quesData[i] [4];
+  };
+}
+
+
+
+
+// function secondQuestion() {
+//   var ans2 = prompt('Do I have a cat?');
+//   if (ans2.toUpperCase() === 'Y' || ans2.toUpperCase() === 'YES') {
+//     res2.textContent = 'No, I do not have a cat.';
+//   } else {
+//     res2.textContent = 'You are right.'
+//   }
+// }
+//
+// secondQuestion()
+//
+//
+//
+// function thirdQuestion() {
+//   var ans3 = prompt('Do I live in Kent?');
+//   if (ans3.toUpperCase() === 'Y' || ans3.toUpperCase() === 'YES') {
+//     res3.textContent = 'You are right.';
+//   } else {
+//     res3.textContent = 'You are wrong.';
+//   }
+// }
+//
+// thirdQuestion()
+//
+//
+//
+// function fourthQuestion() {
+//   var ans4 = prompt('Was I born in Seattle?')
+//   if (ans4.toUpperCase() === 'Y' || ans4.toUpperCase() === 'YES') {
+//     res4.textContent = 'No, I was born in India';
+//   } else {
+//     res4.textContent = 'You are right.';
+//   }
+// }
+//
+// fourthQuestion()
